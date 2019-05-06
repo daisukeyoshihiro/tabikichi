3748fc6ad1e54318c918e90c700207e456b749e2
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-06 13:05:44 +0900

    created signup function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-      if @user.save
-        redirect_to root_url
-      else
-        render 'new'
-      end
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def show
