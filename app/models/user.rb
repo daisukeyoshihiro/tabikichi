5d3a26890f7dc5a2bb427b0fa424ee8dcfca0f6a
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-07 10:59:25 +0900

    fixed userpages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     validates :name, presence: true, length: {minimum:2, maximum:30}
     validates :email, presence: true, uniqueness: true
     has_secure_password
-    validates :password, length: { minimum:6 }
+    validates :password, length: { minimum:6 },
         allow_nil: true
     mount_uploader :picture, PictureUploader
 end
