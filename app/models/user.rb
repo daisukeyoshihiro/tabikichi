773fca24430405b960bcd745812eddd4d530fb4e
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-07 10:30:15 +0900

    created userpages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    validates :name, presence: true, length: {minimum:2, maximum:12}
+    validates :name, presence: true, length: {minimum:2, maximum:30}
     validates :email, presence: true, uniqueness: true
     has_secure_password
     validates :password, length: { minimum:6 }
+    mount_uploader :picture, PictureUploader
 end
