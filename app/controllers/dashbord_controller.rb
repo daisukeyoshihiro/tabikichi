9d331680da2c674c5e28d2f23d475a80e8fb268c
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-21 12:47:44 +0900

    fix activity function

--- app/controllers/dashbord_controller.rb
+++ app/controllers/dashbord_controller.rb
@@ -1,6 +1,6 @@
 class DashbordController < ApplicationController
-	def index
-		@users = current_user.active_relationships.collect{ |n| n.followed_id}
-		@activities = Activity.where("user_id IN (?) OR user_id = ?", @users, current_user.id).order(created_at: :desc).paginate(page: params[:page], per_page: 8)
-	end
+  def index
+    @users = current_user.active_relationships.collect{ |n| n.followed_id}
+    @activities = Activity.where("user_id IN (?) OR user_id = ?", @users, current_user.id).order(created_at: :desc).paginate(page: params[:page], per_page: 8)
+  end
 end
