3b87fc2bd87e9e13cd2c26e258d0e7834aeda392
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-21 12:29:27 +0900

    create activity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @activities = @user.activities.order(created_at: :desc).paginate(page: params[:page], per_page: 7)
   end
 
   def edit
