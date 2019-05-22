036399871c0f253e5feb3f3fb4d809941f0d8571
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-22 10:55:11 +0900

    create learned function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,12 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+    if params[:status] == nil
+      @categories = Category.all
+    elsif params[:status] == 'learned'
+      @categories = current_user.lessons.where.not(result: nil).collect { |c| c.category }
+      # @categories = Category.joins(:lessons) is also possible to display
+    else
+      @categories = Category.left_outer_joins(:lessons).merge(Lesson.where(id: nil))
+    end
   end
 end
