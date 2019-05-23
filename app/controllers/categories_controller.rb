a587d1bd68b36c8dba3809f4800a32393fe997ba
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-23 12:13:49 +0900

    I added user's info on learned function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,9 +4,9 @@ class CategoriesController < ApplicationController
       @categories = Category.joins(:words)
     elsif params[:status] == 'learned'
       @categories = current_user.lessons.where.not(result: nil).collect { |c| c.category }
-      #@categories = Category.joins(:lessons is also possible to display
     else
-      @categories = Category.left_outer_joins(:lessons).merge(Lesson.where(result: nil)).joins(:words)
+      #@categories = Category.left_outer_joins(:lessons).merge(Lesson.where(result: nil)).joins #without user's info
+      @categories = Category.joins("LEFT OUTER JOIN lessons ON categories.id = lessons.category_id AND lessons.user_id = #{current_user.id}").where(lessons: {result: nil}).joins(:words)
     end
   end
 end
