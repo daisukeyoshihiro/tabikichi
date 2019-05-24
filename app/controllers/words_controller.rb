7f40d7b137a93d5d1884d99a1d24497956cf6be3
daisukeyoshihiro <cape.jasm1ne3@gmail.com>
2019-05-24 11:37:35 +0900

    create category filter

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,13 @@
 class WordsController < ApplicationController
   def index
+    @categories = Category.all
+    if params[:category] == nil
+      @answers = current_user.answers.order(created_at: :desc).paginate(page: params[:page], per_page: 8)
+      @title = 'All learned'
+    else
+      category = Category.find(params[:category])
+      @answers = category.answers.where(lessons: {user_id: current_user.id}).order(created_at: :desc).paginate(page: params[:page], per_page: 8)
+      @title = category.title
+    end
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :words, dependent: :destroy
   has_many :lessons, dependent: :destroy
+  has_many :answers, through: :lessons
   validates :title, presence: true, length: {maximum: 15}
   validates :description, presence: true, length: {maximum: 50}
 end
